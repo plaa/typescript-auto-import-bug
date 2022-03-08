b2e1d6a4d976bb4d4686123b1eeca655ded4c6ee
Sampo Juustila <sampo.juustila@iki.fi>
2022-03-08 16:44:16 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Demonstration of Typescript auto-import bug.
---
+++ src/B.ts
@@ -0,0 +1,5 @@
+import { A } from '.';
+
+// Remove import above and press cmd-space after `A` and it *only* suggests
+// Auto import from '.' class A
+export class B extends A {}
